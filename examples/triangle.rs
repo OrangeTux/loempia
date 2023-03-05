79abfa1f3128d611ec91b9dc520a5d5add734f28
Auke Oosterhoff <auke@orangetux.nl>
2023-03-05 14:00:04 +0100

    Integrate clap in square and triangle

--- examples/triangle.rs
+++ examples/triangle.rs
@@ -1,11 +1,33 @@
-use loempia::point::Coordinate;
-use loempia::{Driver, Error, Plot};
 use std::path::Path;
+use std::path::PathBuf;
 
-fn main() -> Result<(), Error> {
-    let serial_path = Path::new("/dev/ttyACM0");
-    let mut driver = Driver::open(serial_path)?;
+use clap::{Parser, Subcommand};
 
+use loempia::roland_dxy::{default_port_settings, Driver};
+use loempia::{point::Coordinate, Error, Plot};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Plot
+    Plot {
+        /// Path to serial device.
+        #[arg(short, long, default_value = "/dev/ttyUSB0")]
+        device: PathBuf,
+    },
+    Preview {
+        /// Location where SVG is written to.
+        #[arg(short, long, default_value = "/tmp/triangle.svg")]
+        output: PathBuf,
+    },
+}
+
+fn get_plot() -> Plot {
     let path = vec![
         Coordinate::new(1000, 1000),
         Coordinate::new(2000, 0000),
@@ -16,12 +38,25 @@ fn main() -> Result<(), Error> {
         Coordinate::new(3000, 1000),
         Coordinate::new(1000, 1000),
     ];
-    let plot = {
-        let path = path;
-        let paths = loempia::Paths::new(vec![path])?;
-        Plot::new(paths)
-    };
+    Plot::from_path(path).expect("Failed to create Plot.")
+}
 
-    driver.plot(&plot)?;
+fn main() -> Result<(), Error> {
+    let plot = get_plot();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Plot { device } => {
+            let serial_path = Path::new(device);
+            let mut driver = Driver::open(serial_path, default_port_settings())?;
+            driver.plot(&plot)?;
+        }
+        Commands::Preview { output } => {
+            let document = plot.preview();
+            svg::save(output, &document).expect(&format!("Failed to save preview at {}.", output.display()));
+            println!("Preview written to {}.", output.display());
+        }
+    }
+
     Ok(())
 }
