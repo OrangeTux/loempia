995a0b37b622ee4aa17377ab87a19fc12395c512
Auke Willem Oosterhoff <1565144+OrangeTux@users.noreply.github.com>
2023-04-15 15:30:34 +0200

    Add CLI arguments to examples (#11)
    This commit adds command line arguments to the examples.
    
    One can use preview sub command to export the plot to SVG.
    Whereas the subcommand plot plots the plot using the Roland DXY.
    
    Fixes #3

--- examples/square.rs
+++ examples/square.rs
@@ -1,11 +1,33 @@
-use loempia::roland_dxy::{Driver, default_port_settings};
-use loempia::{point::Coordinate, Error, Plot};
 use std::path::Path;
+use std::path::PathBuf;
 
-fn main() -> Result<(), Error> {
-    let serial_path = Path::new("/dev/ttyUSB0");
-    let mut driver = Driver::open(serial_path, default_port_settings())?;
+use clap::{Parser, Subcommand};
 
+use loempia::roland_dxy::{default_port_settings, Driver};
+use loempia::{point::Coordinate, Error, Plot};
+
+#[derive(Parser, Debug)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Plot
+    Plot {
+        /// Path to serial device.
+        #[arg(short, long, default_value = "/dev/ttyUSB0")]
+        device: PathBuf,
+    },
+    Preview {
+        /// Location where SVG is written to.
+        #[arg(short, long, default_value = "/tmp/square.svg")]
+        output: PathBuf,
+    },
+}
+
+fn get_plot() -> Plot {
     let path = vec![
         Coordinate::new(0, 0),
         Coordinate::new(1000, 0),
@@ -13,8 +35,25 @@ fn main() -> Result<(), Error> {
         Coordinate::new(0, 1000),
         Coordinate::new(0, 0),
     ];
-    let plot = Plot::from_path(path)?;
+    Plot::from_path(path).expect("Failed to create Plot.")
+}
 
-    driver.plot(&plot)?;
+fn main() -> Result<(), Error> {
+    let plot = get_plot();
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Plot { device } => {
+            let serial_path = Path::new(device);
+            let mut driver = Driver::open(serial_path, default_port_settings())?;
+            driver.plot(&plot)?;
+        }
+        Commands::Preview { output } => {
+            let document = plot.preview();
+            svg::save(output, &document).expect(&format!("Failed to save preview at {}.", output.display()));
+            println!("Preview written to {}.", output.display());
+        }
+    }
+
     Ok(())
 }
